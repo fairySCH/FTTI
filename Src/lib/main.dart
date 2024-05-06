38213bd047280fbc6d2a16ee1abf299e2455d6be
lee-boseong <tjd406@dgu.edu>
2024-05-06 21:13:00 +0900

    update commit

--- Src/lib/main.dart
+++ Src/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'FTTI',
+      title: 'FTTI00',
       home: Login_Screen(),
     );
   }
